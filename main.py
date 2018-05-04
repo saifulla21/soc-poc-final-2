4b22b7b8677ecba0a52f99150986cf4b755e1e00
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-04 17:25:52 +0530

    use cpu intense

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from os import getenv
 import pymssql
+import time
+from random import randint
+
 
 
 app = Flask(__name__)
@@ -15,26 +18,40 @@ def index():
     
 @app.route('/test')
 def test():
-    server = 'cas-sqldb-dev.database.windows.net'
-    user = 'casroot'
-    password = 'db123$%^'
+   intense_cpu()
+#     server = 'cas-sqldb-dev.database.windows.net'
+#     user = 'casroot'
+#     password = 'db123$%^'
 
-    conn = pymssql.connect(server, user, password, "cas-sqldev-tenant-44")
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM sites where product_type = %s','XenDesktop')
-    row = cursor.fetchone()
-    str_my = ''
-    while row:    
-        str_my = str_my + row[0] + ' , '
-        print("tenant_ID=%s, Name=%s" % (row[0], row[1]))
-        row = cursor.fetchone()
-    print str_my
-    conn.close()
-    return str_my
+#     conn = pymssql.connect(server, user, password, "cas-sqldev-tenant-44")
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM sites where product_type = %s','XenDesktop')
+#     row = cursor.fetchone()
+#     str_my = ''
+#     while row:    
+#         str_my = str_my + row[0] + ' , '
+#         print("tenant_ID=%s, Name=%s" % (row[0], row[1]))
+#         row = cursor.fetchone()
+#     print str_my
+#     conn.close()
+#     return str_my
+    
+ def intense_cpu:
+    x = 1
+    threshold = randint(1, 10)
+    start = int(time.time())
+    while True:
+        delta = int(time.time()) - start
+        print delta
+        if (delta > threshold):
+            break
+        x= x*x
+    return 'threshold : '+str(threshold)
 
 @socketio.on('myevent')
 def test_message(message):
-    emit('myresponse', {'data': message})
+    resp = intense_cpu()
+    emit('myresponse', {'data': message, 'resp': resp})
     
 @socketio.on('chat message')
 def test_message(message):
