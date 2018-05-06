5593eccc6a2c334c464704813bd9079d1fccde53
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-06 18:49:40 +0530

    logger info

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from flask_socketio import SocketIO, emit
 from os import getenv
 import pyodbc
 import time
+import logging
 from random import randint
 
 
-
+logging.basicConfig(filename='soc-poc.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = False
@@ -14,16 +15,19 @@ socketio = SocketIO(app,engineio_logger=False,log_output=False,async_mode='event
 
 @app.route('/')
 def index():
+    logging.info('rendering index page')
     return render_template('index.html')
     
 @app.route('/test')
 def test():
+   logging.info('running intense cpu operation')
    return  intense_cpu()
     
     
 def intense_cpu():
     x = 1
     threshold = randint(1, 10)
+    logging.debug('threshold value', str(threshold))
     start = int(time.time())
     while True:
         delta = int(time.time()) - start
@@ -33,14 +37,16 @@ def intense_cpu():
     return 'threshold : '+str(threshold)
 
 def db_connect():
-    
+    logging.info('connecting to database')
     conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=10.62.136.217;DATABASE=cas-sqldev-tenant-007;UID=sa;PWD=Passw0rd')
     cursor = conn.cursor()
+    logging.info('connected to database')
     cursor.execute('SELECT * FROM sites where product_type = %s','XenDesktop')
     row = cursor.fetchone()
     str_my = ''
     while row:    
         str_my = str_my + row[0] + ' , '
+        logging.info('row set', row[0])
 #         print("tenant_ID=%s, Name=%s" % (row[0], row[1]))
         row = cursor.fetchone()
     conn.close()
@@ -48,11 +54,13 @@ def db_connect():
 
 @socketio.on('myevent')
 def test_message(message):
+    logging.info('socket request received on myevent')
     resp = db_connect()
     emit('myresponse', {'data': message, 'resp': resp})
     
 @socketio.on('chat message')
 def test_message(message):
+    logging.info('socket request received on chatmessage')
     emit('chat message', 'server response')
 
 if __name__ == '__main__':
